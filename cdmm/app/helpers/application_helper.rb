654c4b3cd1971184b32225dc569329c3a5a8ffbc
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-10-23 17:23:56 +0700

    feat: Add custom meta tags

--- cdmm/app/helpers/application_helper.rb
+++ cdmm/app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+    def default_meta_tags
+        {
+            site: 'CDMM',
+            title: 'Continuous Delivery Maturity Model',
+            reverse: true,
+            separator: '|',
+            description: 'Continuous Delivery Maturity Assessment Form',
+            keywords: 'action, horror, drama',
+            canonical: request.original_url,
+            noindex: !Rails.env.production?,
+            icon: [
+                { href: image_url('box.svg'), type: 'image/svg+xml' },
+                # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+            ],
+            og: {
+                site_name: 'CDMM',
+                title: 'Continuous Delivery Maturity Model',
+                description: 'The Continuous Delivery Maturity Assessment Form', 
+                type: 'website',
+                url: request.original_url,
+                # image: image_url('login-page.png')
+            }
+        }
+    end
 end
